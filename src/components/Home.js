7384ce73193670d9ce8c006f2259dffb61b67983
Vishwa prasanth <vishwaprasanth1411@gmail.com>
2023-03-01 19:43:45 +0530

    first commit

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import NavBar from './NavBar';
+
+const Home = () => {
+  return (
+    <div className='home-container'>
+     <NavBar/>
+    </div>
+    
+  )
+}
+
+export default Home;
---
+++ src/components/NavBar.js
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+import Logo from "../assets/Logo.svg";
+import {BsCart2} from "react-icons/bs"
+import { HiOutlineBars3} from "react-icons/hi2"
+import {
+    Box,
+    Drawer,
+    ListItem,
+    ListItemButton,
+    ListItemIcon,
+    ListItemText,
+} from "@mui/material";
+import HomeIcon from '@mui/icons-material/Home';
+import InfoIcon from '@mui/icons-material/Info';
+import CommentRoundedIcon from "@mui/icons-material/CommentRounded";
+import PhoneRounderIcon from '@mui/icons-material/PhoneRounded';
+import ShoppingCartRoundedIcon from "@mui/icons-material/ShoppingCartRounded";
+
+
+const NavBar = () => {
+   
+  const [openMenu, setOpenMenu] = React.useState(false);
+  const menuOption = [
+    {
+        text: "Home",
+        icon:<HomeIcon/>
+    },
+    {
+        text: "About",
+        icon:<InfoIcon/>
+    },
+    {
+        text: "Testimonials",
+        icon:<CommentRoundedIcon/>
+    },
+    {
+        text: "Contact",
+        icon:<PhoneRounderIcon/>
+    },
+    {
+        text: "Cart",
+        icon:<ShoppingCartRoundedIcon/>
+    },
+  ]
+  return (
+    <nav>
+        <div className='nav-logo-container'>
+            <img src={Logo} alt=""/>
+        </div>
+        <div className='navbar-links-container'>
+            <a href=''>Home</a>
+            <a href=''>About</a>
+            <a href=''>Testimonials</a>
+            <a href=''>Contact</a>
+            <a href=''>
+                <BsCart2 className='navbar-cart-icon'/>
+            </a>
+            <button className='primary-button'>Booking Now</button>
+        </div>
+        <div className='navbar-menu-container'>
+            <HiOutlineBars3 onClick={() => setOpenMenu(true)}/>
+        </div>
+        <Drawer open ={openMenu} onClose={()=> setOpenMenu(true) }/>
+    </nav>
+  )
+}
+
+export default NavBar
